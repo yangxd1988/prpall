21e486e120fa90196d26dcc5c34085a1ef47749e
zbc <zbc0512@163.com>
2015-09-09 17:00:14 +0800

    Add project
    After publication of the first submission

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # prpall
-<http://prpall.top>
+
+项目主页：<http://prpall.top>  
+
+本项目使用了：  
+spring-webmvc  
+mybatis  
---
+++ src/com/prpall/controller/LoginController.java
@@ -0,0 +1,15 @@
+package com.prpall.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("login")
+public class LoginController {
+
+    @RequestMapping("login.do")
+    public String login() {
+
+        return "login";
+    }
+}
