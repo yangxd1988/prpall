d8f975b2e270fa387cbf6cd7f97e50ee3ef40913
win_zbc <zbc0512@163.com>
2015-09-10 14:19:35 +0800

    update mybatis test of user

--- src/com/prpall/controller/LoginController.java
+++ src/com/prpall/controller/LoginController.java
@@ -1,7 +1,15 @@
 package com.prpall.controller;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.prpall.schema.User;
 
 @Controller
 @RequestMapping("login")
@@ -11,5 +19,30 @@ public class LoginController {
     public String login() {
 
         return "login";
+    }
+
+    public static void main(String[] args) throws IOException {
+        // mybatis的配置文件
+        String resource = "mybatis-config.xml";
+        // 使用类加载器加载mybatis的配置文件（它也加载关联的映射文件）
+        InputStream is = LoginController.class.getClassLoader().getResourceAsStream(resource);
+        // 构建sqlSession的工厂
+        SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(is);
+        // 使用MyBatis提供的Resources类加载mybatis的配置文件（它也加载关联的映射文件）
+        // Reader reader = Resources.getResourceAsReader(resource);
+        // 构建sqlSession的工厂
+        // SqlSessionFactory sessionFactory = new
+        // SqlSessionFactoryBuilder().build(reader);
+        // 创建能执行映射文件中sql的sqlSession
+        SqlSession session = sessionFactory.openSession();
+        /**
+         * 映射sql的标识字符串，
+         * me.gacl.mapping.userMapper是userMapper.xml文件中mapper标签的namespace属性的值，
+         * getUser是select标签的id属性值，通过select标签的id属性值就可以找到要执行的SQL
+         */
+        String statement = "mybatis.mapper.UserMapper.selectUser";// 映射sql的标识字符串
+        // 执行查询返回一个唯一user对象的sql
+        User user = session.selectOne(statement, 1);
+        System.out.println(user);
     }
 }
---
+++ src/com/prpall/schema/User.java
@@ -0,0 +1,23 @@
+package com.prpall.schema;
+
+public class User {
+
+    private String userName;
+    private String userPasswd;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPasswd() {
+        return userPasswd;
+    }
+
+    public void setUserPasswd(String userPasswd) {
+        this.userPasswd = userPasswd;
+    }
+}
